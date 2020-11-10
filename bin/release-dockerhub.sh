820baad620d1a5ba805db7cfa841e55f39374e9e
Don Brown <mrdon@twdata.org>
2020-11-09 22:46:27 -0700

    Trying a different tagging strategy

--- bin/release-dockerhub.sh
+++ bin/release-dockerhub.sh
@@ -4,9 +4,9 @@ docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_API_KEY
 
 VERSION=`python setup.py --version`
 echo "Building $VERSION"
-docker build -t code-video-generator:$VERSION --build-arg VERSION=$VERSION .
-docker tag code-video-generator:$VERSION mrdonbrown/code-video-generator:$VERSION
-docker tag code-video-generator:$VERSION mrdonbrown/code-video-generator:latest
+docker build -t code-dev:$VERSION --build-arg VERSION=$VERSION .
+docker tag code-dev:$VERSION mrdonbrown/code-video-generator:$VERSION
+docker tag code-dev:$VERSION mrdonbrown/code-video-generator:latest
 
 echo "Pushing $VERSION"
 docker push mrdonbrown/code-video-generator:$VERSION
