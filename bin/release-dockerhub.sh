bd3a496fef8f76c959cdefbf8086048d6d8c1cbb
Don Brown <mrdon@twdata.org>
2020-11-09 22:48:14 -0700

    Ok, fixing docker release for sure now

--- bin/release-dockerhub.sh
+++ bin/release-dockerhub.sh
@@ -4,7 +4,7 @@ docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_API_KEY
 
 VERSION=`python setup.py --version`
 echo "Building $VERSION"
-docker build -t code-dev:$VERSION --build-arg VERSION=$VERSION .
+docker build -t code-dev:$VERSION --build-arg VERSION=$VERSION -f docker/Dockerfile .
 docker tag code-dev:$VERSION mrdonbrown/code-video-generator:$VERSION
 docker tag code-dev:$VERSION mrdonbrown/code-video-generator:latest
 
